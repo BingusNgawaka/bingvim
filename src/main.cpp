92fe3a827a8012fdfd36e813f68a84a0a0f012cd
Willow Ngawaka <eth.ecn@hotmail.com>
2026-03-02 11:51:22 +1100

    window scrolling works, just need to confine cursor to valid text

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cstdio>
 #include <fstream>
 #include <iostream>
@@ -5,7 +6,8 @@
 #include <string>
 #include <vector>
 
-#define SCROLL_BUFFER 6
+#define SCROLL_BUFFER_Y 8
+#define SCROLL_BUFFER_X 8
 
 template <typename T>
 struct Vec2{
@@ -51,17 +53,6 @@ Vec2<int> getTerminalSize(){
 }
 
 /*
-
-struct Cursor{
-    Vec2<int> pos;
-    Vec2<int> offset;
-    char mode {'n'};
-    char last_input {};
-    std::string filepath {};
-    std::string command {};
-    bool running {true};
-};
-
 void write_file(std::vector<std::vector<char>> lines, std::string filepath){
     std::ofstream file(filepath, std::ios::trunc);
 
@@ -73,202 +64,6 @@ void write_file(std::vector<std::vector<char>> lines, std::string filepath){
     }
 
     file.close();
-}
-
-void show_screen(std::vector<std::vector<char>> lines, Cursor& cursor){
-    Vec2<int> term {get_terminal_size()};
-
-    // handle screen scrolling
-    int visual_curs_x {cursor.pos.x - cursor.offset.x};
-    int total_pad = MARGIN+XPAD;
-    if(visual_curs_x + total_pad > term.x)
-        cursor.offset.x = cursor.pos.x + total_pad - term.x;
-    if(cursor.offset.x > 0 && visual_curs_x < total_pad){
-        cursor.offset.x = cursor.pos.x - total_pad;
-    }
-
-    int visual_curs_y {cursor.pos.y - cursor.offset.y};
-    if(visual_curs_y + YPAD > term.y)
-        cursor.offset.y = cursor.pos.y + YPAD - term.y;
-    if(cursor.offset.y > 0 && visual_curs_y < YPAD){
-        cursor.offset.y = cursor.pos.y - YPAD;
-    }
-    //
-
-    for(int i{cursor.offset.y}; i < term.y + cursor.offset.y - 2; ++i){
-        // do margin and line numbers
-        int digit_offset {2};
-        int line_num {std::abs(cursor.pos.y - i)};
-        if(cursor.pos.y == i){
-            digit_offset++;
-            line_num = i+1;
-        }
-
-        int num {line_num};
-        while(num >= 10){
-            num /= 10;
-            digit_offset++;
-        }
-        //
-
-        for(int j{cursor.offset.x}; j < term.x + cursor.offset.x; ++j){
-            if(i < lines.size()){
-                attron(COLOR_PAIR(2));
-                mvprintw(i-cursor.offset.y, MARGIN - digit_offset, "%d", line_num);
-                attron(COLOR_PAIR(1));
-                if(j < lines.at(i).size()){
-                    char c {lines.at(i).at(j)};
-                    mvprintw(i-cursor.offset.y, j-cursor.offset.x+MARGIN, "%c", c);
-                }
-            }
-        }
-    }
-
-    mvprintw(term.y-2, 1, "Editing: %s", cursor.filepath.c_str());
-    mvprintw(term.y-2, term.x - 40, "Cursor: %d, %d", cursor.pos.x, cursor.pos.y);
-
-
-    //if(cursor.writingCommand){
-     //   mvprintw(term.y-1, 0, " Command: ");
-    //}
-}
-
-void handle_normal_mode(std::vector<std::vector<char>>& lines, Cursor& cursor){
-    // normal mode movement
-    if(lines.size() > 0){
-        if(cursor.last_input == 'h' && cursor.pos.x > 0)
-            cursor.pos.x -= 1;
-        if(cursor.last_input == 'j' && cursor.pos.y < lines.size()-1)
-            cursor.pos.y += 1;
-        if(cursor.last_input == 'k' && cursor.pos.y > 0)
-            cursor.pos.y -= 1;
-        if(cursor.last_input == 'l')// && cursor.pos.x < lines.at(cursor.pos.y).size()-1 && lines.at(cursor.pos.y).size() > 0)
-            cursor.pos.x += 1;
-
-        if(cursor.pos.x > lines.at(cursor.pos.y).size()-1 || lines.at(cursor.pos.y).size() == 0){
-            cursor.pos.x = std::max(0, static_cast<int>(lines.at(cursor.pos.y).size()-1));
-        }
-    }
-
-    if(cursor.last_input == 'i'){
-        cursor.mode = 'i';
-        return;
-    }
-    if(cursor.last_input == 'a'){
-        cursor.mode = 'i';
-        if(lines.at(cursor.pos.y).size() > 0)
-            cursor.pos.x++;
-        return;
-    }
-    if(cursor.last_input == 'A'){
-        cursor.mode = 'i';
-        cursor.pos.x = lines.at(cursor.pos.y).size();
-        return;
-    }
-    if(cursor.last_input == 'O'){
-        int offset {std::max(cursor.pos.y, 0)};
-        lines.insert(lines.begin() + offset, std::vector<char>());
-        cursor.pos.x = 0;
-        cursor.mode = 'i';
-        return;
-    }
-    if(cursor.last_input == 'o'){
-        lines.insert(lines.begin() + cursor.pos.y + 1, std::vector<char>());
-        cursor.pos.x = 0;
-        cursor.pos.y++;
-        cursor.mode = 'i';
-        return;
-    }
-    if(cursor.last_input == ':'){
-        cursor.command.clear();
-        cursor.mode = 'c'; // c for command
-        return;
-    }
-}
-
-void handle_input_mode(std::vector<std::vector<char>>& lines, Cursor& cursor){
-    // Escape is pressed
-    if(cursor.last_input == 27){
-        if(cursor.pos.x > 0)
-            cursor.pos.x--;
-        cursor.mode = 'n';
-        return;
-    }
-
-    // Backspace is pressed
-    if(cursor.last_input == 127){
-        if(cursor.pos.x == 0 && cursor.pos.y == 0)
-            return;
-
-        // handle deleting a line
-        if(cursor.pos.x == 0){
-            cursor.pos.x = lines.at(cursor.pos.y-1).size();
-            lines.at(cursor.pos.y-1).insert(lines.at(cursor.pos.y-1).end(), lines.at(cursor.pos.y).begin(), lines.at(cursor.pos.y).end());
-            lines.erase(lines.begin()+cursor.pos.y);
-            cursor.pos.y--;
-            return;
-        }
-
-        cursor.pos.x--;
-        lines.at(cursor.pos.y).erase(lines.at(cursor.pos.y).begin()+cursor.pos.x);
-        return;
-    }
-
-    // Enter is pressed
-    if(cursor.last_input == 10){
-        lines.insert(lines.begin() + cursor.pos.y + 1, std::vector<char>());
-        if(cursor.pos.x < lines.at(cursor.pos.y).size()){
-            for(int i{cursor.pos.x}; i < lines.at(cursor.pos.y).size();){
-                lines.at(cursor.pos.y+1).push_back(lines.at(cursor.pos.y).at(i));
-                lines.at(cursor.pos.y).erase(lines.at(cursor.pos.y).begin() + i);
-            }
-        }
-        cursor.pos.x = 0;
-        cursor.pos.y++;
-        return;
-    }
-
-    // todo:
-    // Del is pressed
-    // Arrow keys are pressed
-    // double check remaining input as writable chars
-    // including esc codes i think?
-    lines.at(cursor.pos.y).insert(lines.at(cursor.pos.y).begin()+cursor.pos.x, cursor.last_input);
-    cursor.pos.x++;
-}
-
-void handle_command_mode(std::vector<std::vector<char>>& lines, Cursor& cursor){
-    // Escape is pressed
-    if(cursor.last_input == 27)
-        cursor.mode = 'n';
-
-    // Backspace is pressed
-    if(cursor.last_input == 127)
-        cursor.command.pop_back();
-
-    // Enter is pressed
-    if(cursor.last_input == 10){
-        // run cmd
-        cursor.mode = 'n';
-        if(cursor.command.substr(0,2) == "w "){
-            write_file(lines, cursor.command.substr(2));
-        }
-        if(cursor.command == "q"){
-            cursor.running = false;
-        }
-        if(cursor.command == "wq"){
-            write_file(lines, cursor.command.substr(2));
-            cursor.running = false;
-        }
-    }
-
-    // todo:
-    // Del is pressed
-    // Arrow keys are pressed
-    // double check remaining input as writable chars
-    // including esc codes i think?
-
-    cursor.command.push_back(cursor.last_input);
 }
 */
 
@@ -297,7 +92,7 @@ struct Window{
 
     WINDOW* winPtr; // curses window
 
-    std::size_t topLine;
+    Vec2<std::size_t> topLeft;
     Vec2<int> viewportSize;
 };
 
@@ -310,7 +105,8 @@ void refreshWindow(Window& currWin){
     werase(win);
 
     for(std::size_t row{}; static_cast<int>(row) < currWin.viewportSize.y; ++row){
-        mvwprintw(win, static_cast<int>(row), 0, "%s", currWin.buf->lines.at(row+currWin.topLine).c_str());
+        std::string line {currWin.buf->lines.at(row+currWin.topLeft.y)};
+        mvwprintw(win, static_cast<int>(row), 0, "%s", line.substr(std::min(line.size(), currWin.topLeft.x)).c_str());
     }
 
     moveToCursorPos(currWin);
@@ -319,28 +115,38 @@ void refreshWindow(Window& currWin){
 }
 
 void handleBufferInput(Window& win, int ch){
-    if(ch == 'h' && win.cursorPos.x > 0)
-        --win.cursorPos.x;
+    if(ch == 'h'){
+        if(win.cursorPos.x <= SCROLL_BUFFER_X && win.topLeft.x > 0){
+            --win.topLeft.x;
+        }else if(win.cursorPos.x > 0){
+            --win.cursorPos.x;
+        }
+    }
 
     if(ch == 'k'){
-        //scrolling logic todo
-        if(win.cursorPos.y <= SCROLL_BUFFER && win.topLine > 0){
-            --win.topLine;
+        if(win.cursorPos.y <= SCROLL_BUFFER_Y && win.topLeft.y > 0){
+            --win.topLeft.y;
         }else if(win.cursorPos.y > 0){
             --win.cursorPos.y;
         }
     }
 
     if(ch == 'j'){
-        //scrolling logic todo
-        if(win.cursorPos.y >= win.viewportSize.y - (1+SCROLL_BUFFER)){
-            ++win.topLine;
-        }else{
+        bool wantToScroll {win.cursorPos.y >= win.viewportSize.y - (1+SCROLL_BUFFER_Y)};
+        bool canScroll {win.topLeft.y + win.viewportSize.y < win.buf->lines.size()};
+        if(wantToScroll && canScroll){
+            ++win.topLeft.y;
+        }else if(win.cursorPos.y + win.topLeft.y < win.buf->lines.size() - 1){
             ++win.cursorPos.y;
         }
     }
+
     if(ch == 'l'){
-        ++win.cursorPos.x;
+        if(win.cursorPos.x >= win.viewportSize.x - (1+SCROLL_BUFFER_X)){
+            ++win.topLeft.x;
+        }else{
+            ++win.cursorPos.x;
+        }
     }
 }
 
@@ -374,7 +180,7 @@ int main(int argc, char** argv){
 
     std::size_t currWindow {0};
     std::vector<Window> windows{
-        {&buffers[0], {0,0}, newwin(terminalSize.y, terminalSize.x, 0, 0), 0, terminalSize}
+        {&buffers[0], {0,0}, newwin(terminalSize.y, terminalSize.x, 0, 0), {0,0}, terminalSize}
     };
 
     refresh();
