1e3ff5836c666ecaa31dfa4bfde590f329c7c0c6
Willow Ngawaka <eth.ecn@hotmail.com>
2026-03-03 13:44:11 +1100

    added visual cursor to make movement more intuitive

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,11 @@
 #include <cstdio>
 #include <cstdlib>
 #include <fstream>
-#include <iostream>
 #include <ncurses.h>
 #include <string>
 #include <vector>
+
+#define DEBUG false
 
 #define SCROLL_BUFFER_Y 8
 #define SCROLL_BUFFER_X 8
@@ -26,6 +27,9 @@ struct Vec2{
     }
     bool operator==(const Vec2<T>& other) const{
         return (x==other.x && y==other.y);
+    }
+    bool operator!=(const Vec2<T>& other) const{
+        return (x!=other.x || y!=other.y);
     }
 
     void print(std::string vecName = "Vec2"){
@@ -90,28 +94,78 @@ struct Buffer{
 struct Viewport{
     Buffer* buf;
     Vec2<int> cursorPos;
+    Vec2<int> visualCursorPos;
     Vec2<int> scrollPos;
     Vec2<int> size;
+
+    std::string getCurrentLine(){
+        return buf->lines.at(static_cast<std::size_t>(cursorPos.y + scrollPos.y));
+    }
+
+    void boundVisualCursorToText(){
+        std::string line {getCurrentLine()};
+        if(static_cast<std::size_t>(cursorPos.x) >= line.size())
+            visualCursorPos.x = std::max(0, static_cast<int>(line.size()) - 1);
+    }
 
     void moveCursor(Vec2<int> dv){
         // move cursor by dv.x and dv.y
         // if our new cursor loc is outside the range of SCROLL_BUFFER
         // then we clamp our new loc and scroll as much as we can instead
-        Vec2<int> terminalSize {getTerminalSize()};
+        if(dv.x != 0)
+            cursorPos.x = visualCursorPos.x;
+        if(dv.y != 0)
+            cursorPos.y = visualCursorPos.y;
+
         Vec2<int> newPos {cursorPos + dv};
+        // this bounds actual cursor x to line but only if we change x
+        // otherwise you can go one ahead of visual x at the end of a line
+        // and it isnt set to the visual cursor
+        // idk if this is stupid but it works
+        if(dv.x != 0){
+            std::string line {getCurrentLine()};
+            if(static_cast<std::size_t>(newPos.x) >= line.size())
+                newPos.x = std::max(0, static_cast<int>(line.size()) - 1);
+        }
 
         // scroll bound changes as we reach the edge of the screen (scrollPos.x/y = 0)
         Vec2<int> scrollBound {(scrollPos.x > 0) ? SCROLL_BUFFER_X : 0, (scrollPos.y > 0) ? SCROLL_BUFFER_Y : 0};
-        cursorPos.x = std::clamp(newPos.x, scrollBound.x, terminalSize.x - (1 + SCROLL_BUFFER_X));
-        cursorPos.y =  std::clamp(newPos.y, scrollBound.y, terminalSize.y - (1 + SCROLL_BUFFER_Y));
+        cursorPos.x = std::clamp(newPos.x, scrollBound.x, size.x - (1 + SCROLL_BUFFER_X));
+        cursorPos.y =  std::clamp(newPos.y, scrollBound.y, size.y - (1 + SCROLL_BUFFER_Y));
 
         Vec2<int> finalDiff {newPos - cursorPos};
         scrollPos.x = std::max(scrollPos.x + finalDiff.x, 0);
         scrollPos.y = std::max(scrollPos.y + finalDiff.y, 0);
+
+        visualCursorPos = cursorPos;
+
+        boundVisualCursorToText();
     }
 
+    // movement keybinds that i actually use
+    // DONE:
+    // hjkl
+    // TODO:
+    // w, e, b,
+    // w jumps to next beggining of word, e to next end of word, b to the prev beggining of word
+    // word delimiters: not (a-zA-Z, 0-9, _)
+    // WORD delimiters: only whitespace
+    // f, F with ; and ,
+    // / search does a regex search sooo
+    // $ end of line
+    // ^ beggining of text in line
+    //
+    // edit keybinds
+    // i, a, o, I, A, O
+    //
+    // paste buffer related keybinds
+    // yy - yank curr line
+    // nyy - yank n lines below
+    // 
+    //
+    // numbers to denote repeated commands
     void handleBufferInput(int ch){
-        int movementAmnt {2};
+        int movementAmnt {1};
         if(ch == 'h')
             moveCursor({-movementAmnt, 0});
         if(ch == 'j')
@@ -130,17 +184,20 @@ struct Pane{
     void render(){
         werase(window);
 
-        for(std::size_t row{}; static_cast<int>(row) < view.size.y; ++row){
-            std::string line {view.buf->lines.at(row+view.scrollPos.y)};
-            mvwprintw(window, static_cast<int>(row), 0, "%s", line.substr(std::min(static_cast<int>(line.size()), view.scrollPos.x)).c_str());
+        for(int row{}; row < view.size.y; ++row){
+            std::string line {view.buf->lines.at(static_cast<std::size_t>(row+view.scrollPos.y))};
+            mvwprintw(window, static_cast<int>(row), 0, "%s", line.substr(std::min((line.size()), static_cast<std::size_t>(view.scrollPos.x))).c_str());
         }
 
-        mvwprintw(window, view.cursorPos.y, view.cursorPos.x, "+-----------------+");
-        mvwprintw(window, view.cursorPos.y+1, view.cursorPos.x, "  cursorPos(%d, %d)  ", view.cursorPos.x, view.cursorPos.y);
-        mvwprintw(window, view.cursorPos.y+2, view.cursorPos.x, "  scrollPos(%d, %d)  ", view.scrollPos.x, view.scrollPos.y);
-        mvwprintw(window, view.cursorPos.y+3, view.cursorPos.x, "+-----------------+");
+        if(DEBUG){
+            mvwprintw(window, view.cursorPos.y, view.cursorPos.x, "+-----------------+");
+            mvwprintw(window, view.cursorPos.y+1, view.cursorPos.x, "  cursorPos(%d, %d)  ", view.cursorPos.x, view.cursorPos.y);
+            mvwprintw(window, view.cursorPos.y+2, view.cursorPos.x, "  visualPos(%d, %d)  ", view.visualCursorPos.x, view.visualCursorPos.y);
+            mvwprintw(window, view.cursorPos.y+3, view.cursorPos.x, "  scrollPos(%d, %d)  ", view.scrollPos.x, view.scrollPos.y);
+            mvwprintw(window, view.cursorPos.y+4, view.cursorPos.x, "+-----------------+");
+        }
 
-        wmove(window, view.cursorPos.y, view.cursorPos.x);
+        wmove(window, view.visualCursorPos.y, view.visualCursorPos.x);
         wrefresh(window);
     }
 };
@@ -213,7 +270,8 @@ int main(int argc, char** argv){
 
     std::size_t currWindow {0};
     std::vector<Viewport> windows{
-        {&buffers[0], {0,0}, {0,0}, terminalSize}
+        // bound buffer, actualCursorPos, visualCursorPos, scrollPos, viewportSize
+        {&buffers[0], {0,0}, {0,0}, {0,0}, terminalSize}
     };
 
     std::size_t currPane {0};
